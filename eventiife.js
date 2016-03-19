6f914097eb66c68acf5a0dac8671a565ed056ba2
MATT BRUTON <the.bruton@gmail.com>
2016-03-19 15:31:51 -0500

    notes added to keep track of which functions i need to build in eventiife.js

--- eventiife.js
+++ eventiife.js
@@ -1,5 +1,14 @@
 var CarLot = (function(newCarLot){
 
-  
+  // click event for clicking on specific car div
+
+  // click event for clearing text input in navbar and putting cursor in text input box
+
+  // keypress event for changing decription to be equal to input
+
+
+
+
+
 return newCarLot;
 })(CarLot||{});
