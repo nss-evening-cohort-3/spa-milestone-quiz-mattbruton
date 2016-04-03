45731b5dff4311e2f05ef39b16724ab8ab57dee6
MATT BRUTON <the.bruton@gmail.com>
2016-04-02 21:55:55 -0500

    made populatePage function significantly more DRY

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 function populatePage(inventory) {
 
     var htmlContainer = document.getElementById('container');
-
+    var sold = " sold";
     var carString;
 
     // Loop over the inventory and populate the DOM.
@@ -12,35 +12,25 @@ function populatePage(inventory) {
         to add an overlapping image to make it clear to the user that the car isn not available for 
         purchase. */
 
-        if (inventory[i].purchased) {
-            carString = `<div class="col-sm-4 car sold" style="border: 3px solid ${inventory[i].color};">`;
+            carString = `<div class="col-sm-4 car`;
+            if (inventory[i].purchased) {
+                carString += sold;
+            }
+            carString += `" style="border: 3px solid ${inventory[i].color};">`;
             carString += `<h1>${inventory[i].make}</h1>`;
             carString += `<h2>${inventory[i].model}</h2>`;
             carString += `<h4>Year: ${inventory[i].year}</h4>`;
             carString += `<h4>Price: $${inventory[i].price}</h4>`;
             carString += `<p>${inventory[i].description}</p>`;
-            carString += `<img class="soldImg" src="sold.png">`;
-            carString += `</div>`;
-            htmlContainer.innerHTML += carString;
-
-        } else {
-
-            /* Assuming the purchased key's value is false, this is the format for the div containing the car's
-            information. */
-
-            carString = `<div class="col-sm-4 car" style="border: 3px solid ${inventory[i].color};">`;
-            carString += `<h1>${inventory[i].make}</h1>`;
-            carString += `<h2>${inventory[i].model}</h2>`;
-            carString += `<h4>Year: ${inventory[i].year}</h4>`;
-            carString += `<h4>Price: $${inventory[i].price}</h4>`;
-            carString += `<p>${inventory[i].description}</p>`;
+            if (inventory[i].purchased) {
+                carString += `<img class="soldImg" src="sold.png">`;
+            }
             carString += `<img class="blankImg" src="blank.png">`;
             carString += `</div>`;
             htmlContainer.innerHTML += carString;
-        };
 
         // Now that the DOM is loaded, establish all the event listeners needed
-    }
+    };
 
     CarLot.activateEvents();
 
